409d01c8b54f2ededc17bb18525a2036f1dd2e3c
Gustavo Gama <gustavogama494@gmail.com>
2023-12-19 06:27:47 -0300

    Melhoria no logger no pacote de teste do sha512

--- pkg/pacotes/encryption/sha512_test.go
+++ pkg/pacotes/encryption/sha512_test.go
@@ -11,44 +11,52 @@ import (
 func TestMain(m *testing.M) {
 	inicializar.InicializarParaTestes()
 	exitCode := m.Run()
+	if exitCode == 0 {
+		logger.Logger().Info("Testes do pacote encryption executados com sucesso!")
+	} else {
+		logger.Logger().Alerta("Ocorreram erros ao executar os testes do pacote encryption")
+	}
 	os.Exit(exitCode)
 }
 
 func TestGerarSHA512(t *testing.T) {
+	t.Parallel()
 	data := "password123"
 	hash, err := GerarSHA512(data)
 	if err != nil {
-		t.Errorf("Erro ao gerar a hash Sha512: %v", err)
-		logger.Logger().Error("Erro ao gerar a hash Sha512", err)
+		logger.Logger().Error("Teste "+t.Name()+":	Ocorreu um erro ao executar a função GerarSHA512", err)
+		t.FailNow()
 	}
 
 	if hash == "" {
-		t.Error("O hash gerado não deve estar vazio")
-		logger.Logger().Error("O hash gerado não deve estar vazio", err)
+		logger.Logger().Error("Teste "+t.Name()+":	A função GerarSHA512 não retornou qualquer valor", err)
+		t.FailNow()
 	}
-	logger.Logger().Info("Teste TestGerarSHA512 executado com sucesso!")
+	logger.Logger().Info("Teste " + t.Name() + ":	Executado com sucesso!")
 }
 
 func TestCompararSHA512_CredencialValida(t *testing.T) {
+	t.Parallel()
 	hash := "bed4efa1d4fdbd954bd3705d6a2a78270ec9a52ecfbfb010c61862af5c76af1761ffeb1aef6aca1bf5d02b3781aa854fabd2b69c790de74e17ecfec3cb6ac4bf"
 	decryptedPassword := "password123"
 
 	err := CompararSHA512(hash, decryptedPassword)
 	if err != nil {
-		t.Errorf("Erro ao comparar hashes SHA512: %v", err)
-		logger.Logger().Error("Erro ao comparar hashes SHA512", err)
+		logger.Logger().Alerta("Teste " + t.Name() + ":	A função CompararSHA512 identificou que a credencial é inválida, no entanto deveria ser valida")
+		t.FailNow()
 	}
-	logger.Logger().Info("Teste TestCompararSHA512_CredenciailValida executado com sucesso!")
+	logger.Logger().Info("Teste " + t.Name() + ":	Executado com sucesso!")
 }
 
 func TestCompararSHA512_CredencialInvalida(t *testing.T) {
+	t.Parallel()
 	hash := "bed4efa1d4fdbd954bd3705d6a2a78270ec9a52ecfbfb010c61862af5c76af1761ffyb1aef6aca1bf5d02b3781aa854fabd2b69c790de74e17ecfec3cb6ac4bf"
 	decryptedPassword := "password123"
 
 	err := CompararSHA512(hash, decryptedPassword)
 	if err == nil {
-		t.Error("Erro esperado ao comparar hashes SHA512, mas não ocorreu")
-		logger.Logger().Error("Erro esperado ao comparar hashes SHA512, mas não ocorreu", err)
+		logger.Logger().Alerta("Teste " + t.Name() + ":	A função CompararSHA512 identificou que a credencial é valida, no entanto deveria ser inválida")
+		t.FailNow()
 	}
-	logger.Logger().Info("Teste TestCompararSHA512_CredencialInvalida executado com sucesso!")
+	logger.Logger().Info("Teste " + t.Name() + ":	Executado com sucesso!")
 }
